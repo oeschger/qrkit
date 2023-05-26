74fa447010ea2d9ff7c0d2f8e6dbc9b8e3614cc1
IAN OESCHGER <oian@us.ibm.com>
2023-05-26 18:47:07 -0400

    got behavior into bootstrap, nav, templates

--- qrmake_test.py
+++ qrmake_test.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
 @app.route('/generate', methods = ['POST', 'GET'])
 def generate():
     # Data to encode
