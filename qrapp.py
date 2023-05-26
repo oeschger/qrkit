2474556be743182688022f919d5acc0ee71ae923
IAN OESCHGER <oian@us.ibm.com>
2023-05-26 18:48:52 -0400

    cleaning

--- qrapp.py
+++ qrapp.py
@@ -1,9 +1,6 @@
 from flask import Flask, send_file, render_template, request
 import qrcode
 
-# @todo build a form to take submission from / to /generate
-# https://www.digitalocean.com/community/tutorials/how-to-use-web-forms-in-a-flask-application
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -36,11 +33,5 @@ def generate():
     return send_file(fn, mimetype='image/png')
 
 app.run(host='0.0.0.0', port=81)
-
-## prototype for managing args from form submit (?)
-# @app.route(...)
-# def login():
-#     username = request.args.get('username')
-#     password = request.args.get('password')
 
  
